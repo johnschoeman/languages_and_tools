827cba8ca9eba5b9de1dd4a449e8957acbd0fcf7
John Schoeman <johnschoeman1617@gmail.com>
2026-01-25 23:03:28 -0500

    Make cube rotate

--- bevy/bevy_doodles/src/main.rs
+++ bevy/bevy_doodles/src/main.rs
@@ -4,8 +4,12 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, rotate_cube)
         .run();
 }
+
+#[derive(Component)]
+struct RotatingCube;
 
 fn setup(
     mut commands: Commands,
@@ -17,6 +21,7 @@ fn setup(
         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
         MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
         Transform::from_xyz(0.0, 0.5, 0.0),
+        RotatingCube,
     ));
 
     // Light
@@ -33,4 +38,11 @@ fn setup(
         Camera3d::default(),
         Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+}
+
+fn rotate_cube(time: Res<Time>, mut query: Query<&mut Transform, With<RotatingCube>>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_secs());
+        transform.rotate_x(time.delta_secs() * 0.5);
+    }
 }
